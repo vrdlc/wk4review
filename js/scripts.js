aef393c916c63c49e638c82e6452efb1d84da294
vr <student@epicodus.com>
2016-01-29 07:58:12 -0800

    first spec passed automatically, failed second spec

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function PizzaPrice(crust, topping, basePrice) {
+  this.crust = crust;
+  this.topping = topping;
+  this.basePrice = 5;
+}
+
+
+
+
+
+
+
+
+// PizzaPrice.prototye.crust = function(){
+//   if(this.crust === 'x'); {
+//   return this.basePrice;
+//   }
+//    if(this.crust === 'medium'); {
+//     return this.basePrice += 2;
+//   }
+//    if(this.crust === 'large'); {
+//     return this.basePrice += 3;
+//   }
+// }
+//
+// PizzaPrice.prototype.topping = function() {
+//   if(this.topping = 1);
+//   return basePrice += 1;
+//
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,14 @@
 describe("pizzaPrice", function() {
-  it("will change the price of a pizza based on the following properties", function(){
+  it("will create a new pizza based on the following properties", function(){
     var testPrice = new PizzaPrice('small', 'pepperoni');
     expect(testPrice.crust).to.equal('small');
     expect(testPrice.topping).to.equal('pepperoni');
-  });
-});
+  }); //THIS AUTOMATICALLY WORKS IF function PizzaPrice IS BUILT
+
+  it('will add one dollar for every topping added', function() {
+    var testPrice = new PizzaPrice(6)
+      expect(test.PizzaPrice.price).to.equal(6)
+    });
+
+
+}); //END OF DESCRIBE SPEC
