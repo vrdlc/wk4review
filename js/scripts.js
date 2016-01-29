5734e8d5b5ddcc5ff69102fe2ee74645eba30667
vr <student@epicodus.com>
2016-01-29 14:33:52 -0800

    makes idea work, but doesn't use constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,34 @@
-function PizzaPrice(crust, topping, basePrice) {
+function PizzaPrice(crust, topping) {
   this.crust = crust;
   this.topping = topping;
-  this.basePrice = basePrice;
 }
 
-PizzaPrice.prototype.crustPrice = function() {
-  var basePrice = 5;
+PizzaPrice.prototype.crustPrice = function(crustSize) { //MAY BE OBSOLETE
+  var crustPrice = 5;
 
   if (this.crust === 'small') {
-  basePrice += 0;
+  crustPrice += 0;
   } if (this.crust === 'med') {
-    basePrice += 1;
+    crustPrice += 1;
   } if (this.crust === 'lg') {
-    basePrice += 2;
+    crustPrice += 2;
   } if (this.crust === 'xl') {
-    basePrice += 3;
+    crustPrice += 3;
 
-  } return basePrice;
+  } return crustPrice;
 };
 
 
-PizzaPrice.prototype.toppingPrice = function() {
-  var basePrice = 5;
+PizzaPrice.prototype.toppingPrice = function(toppingCost) { //MAY BE OBSOLETE
+  var toppingPrice = 5;
   if (this.topping === 'pepperoni') {
-    basePrice += 1;
+    toppingPrice += 1;
   } if (this.topping === 'mushrooms') {
-    basePrice += 1;
-  } return basePrice;
+    toppingPrice += 1;
+  } return toppingPrice;
 };
 
-PizzaPrice.prototype.finalPrice = function() {
+PizzaPrice.prototype.finalPrice = function() { //THIS WORKS FOR EVERYTHING BUT MIGHT STILL NEED TO USE CRUSTPRICE AND TOPPINGPRICE TO ADD VALUES TOGETHER?
   var basePrice = 5;
 
   if (this.crust === 'small') {
@@ -54,17 +53,66 @@ PizzaPrice.prototype.finalPrice = function() {
 
 }
 
-
 $(document).ready(function(){
-  $("#crust, #topping1").change(function(){
 
-      concatenated_string = parseInt($("#crust").val()) + parseInt($("#topping1").val());
+  $('#crust').click(function(){
 
-  $("#submitbutton").click(function(event){
-    $("#finalPrice").text(concatenated_string)
-    // $(".finalPrice").append(finalPrice)
   });
-});
+
+  $('#topping1').click(function(){
+
+  });
+    $("#crust, #topping1").change(function(){
+
+        finalPrice = parseInt($("#crust").val()) + parseInt($("#topping1").val());
+
+    $("#submitbutton").click(function(event){
+
+      $("#finalPrice").text("Your total is: $" + finalPrice + ".00")
+      $(".finalPrice").append(finalPrice)
+    });
 
 
+
 });
+});
+// $(document).ready(function(){
+//   var pizza = new PizzaPrice();
+//   $('.crust').click(function() {
+//     var this.crustSize = $(this).val();
+//     pizza.crustPrice(crustSize)
+// //     alert(crustSize);
+// //
+// //
+// //
+// //     // pizza.crustPrice(crustPrice);
+// //     // alert(crustPrice);
+//     });
+// //
+//   $('.topping1').click(function(){
+//     var toppingCost = $(this).val();
+//     pizza.toppingPrice(toppingCost);
+// //     alert(toppingCost)
+//   });
+// //
+// //   $("#submitbutton").click(function(crustPrice, toppingPrice) {
+// //     // var crustPrice = "1"
+// //     // var toppingPrice = "1"
+// //
+//     // var finalPrice = crustPrice + toppingPrice;
+//     // $("#finalPrice").text("Your total is: $" + finalPrice + ".00")
+//     // $(".finalPrice").append(pizza.finalPrice)
+//
+//
+//   $("#crust, #topping1").change(function(){
+//
+//       finalPrice = parseInt($("#crust").val()) + parseInt($("#topping1").val());
+//
+//   $("#submitbutton").click(function(event){
+//     $("#finalPrice").text("Your total is: $" + finalPrice + ".00")
+//     $(".finalPrice").append(finalPrice)
+//   });
+//
+// });
+//
+// });
